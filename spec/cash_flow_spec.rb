9de992733865322277fcb802026bc66ed4a7dec0
XykoDevops <xykodevops@gmail.com>
2023-05-05 19:09:21 -0300

    Reescrevendo a descrição dos testes.

--- spec/cash_flow_spec.rb
+++ spec/cash_flow_spec.rb
@@ -5,7 +5,7 @@ require_relative '../cash_flow.rb'
 set :environment, :test
 
 describe 'Transaction' do
-  it 'should initialize correctly' do
+  it 'inicializando corretamente' do
     t = Transaction.new(50, '2023-05-05', 'description')
     expect(t.amount).to eq 50
     expect(t.date).to eq '2023-05-05'
@@ -14,13 +14,13 @@ describe 'Transaction' do
 end
 
 describe 'Debit' do
-  it 'should be a Transaction' do
+  it 'testando a transação de débito' do
     expect(Debit.new(50, '2023-05-05', 'description')).to be_a Transaction
   end
 end
 
 describe 'Credit' do
-  it 'should be a Transaction' do
+  it 'testando a transação de crébito' do
     expect(Credit.new(50, '2023-05-05', 'description')).to be_a Transaction
   end
 end
@@ -32,24 +32,24 @@ describe 'Cashflow App' do
     Sinatra::Application
   end
 
-  it "should display the homepage" do
+  it "Apresentação da home 100%" do
     get '/'
     expect(last_response).to be_ok
   end
 
-  it "should display the summary page" do
+  it "Apresentação do Sumário 100%" do
     get '/summary'
     expect(last_response).to be_ok
   end
 
-  it "should store a transaction and redirect to homepage" do
+  it "Testa a transação ok e redirecionamento para home" do
     post '/cashflow', params={amount: 50, description: 'description', type: 'C', date: '2023-05-05'}
     expect(last_response.redirect?).to be true
     follow_redirect!
     expect(last_request.path).to eq('/')
   end
 
-  it "should display a day summary" do
+  it "Apresentação do Somário Diário 100%" do
     post '/cashflow', params={amount: 50, description: 'description', type: 'C', date: '2023-05-05'}
     get '/summary/2023-05-05'
     expect(last_response).to be_ok
